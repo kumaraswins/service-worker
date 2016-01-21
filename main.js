bef782924a038e947437f6aa2ee7205e59e9b68e
Aswinkumar Santhanam <aswinkumar@tagitmobile.com>
2016-01-21 11:36:06 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+if ('serviceWorker' in navigator) {
+//You can call register every time a page loads without concern; the browser will figure out if the service worker is already registered or not and handle it accordingly.
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+ var jsonFlickrApi = function (response) {
+     var cats = $("#cats");
+	$.each(response.photos.photo, function(i, item) {
+		var src = "https://farm"
+			+ item.farm
+			+ ".static.flickr.com/"
+			+ item.server
+			+ "/" + item.id
+			+ "_" + item.secret
+			+ "_n.jpg";
+		var container = $("<li/>").appendTo(cats);
+		$("<img/>").attr("src", src).appendTo(container);
+	});
+ };
+
+ var flickr_api_key = "208dae30f9b67f9b1870a81f5feb0328";
+ var flickr_api_url = "https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=" + flickr_api_key;
+ $.ajax({
+     url: flickr_api_url,
+     method: 'GET',
+     dataType: 'jsonp',
+     jsonpCallback: 'jsonFlickrApi',
+     cache: true,
+     data: {
+         tags: 'cat',
+         safe_search: 1,
+         per_page: 20
+     },
+     error: function (err) {
+         console.log(err);
+     }
+ });
---
+++ sw.js
@@ -0,0 +1,52 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/jq.js',
+  '/main.css',
+  '/main.js',
+];
+
+self.addEventListener('install', function (event) {
+    
+    //Here you can see we call caches.open with our desired cache name, after this we call cache.addAll and pass in our array of files. This is a chain of promises (caches.open and cache.addAll). event.waitUntil takes a promise and uses it to know how long installation takes, and whether it succeeded. 
+    //This will execute only once
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log('Opened cache'); 
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            return response;
+          }
+        );
+      })
+    );
+});
